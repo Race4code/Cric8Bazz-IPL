fdc1247ed53d1dd3c41da08996af83c05d1eb928
Rahul _Kumar_ Singh <77785150+Race4code@users.noreply.github.com>
2023-02-08 23:31:14 +0530

    navbar active link Function

--- front-end/cric8bazz-ui/src/Components/Navbar.js
+++ front-end/cric8bazz-ui/src/Components/Navbar.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../Assets/web_logo.png'
-import { Link } from 'react-router-dom'
+import { Link,NavLink } from 'react-router-dom'
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='navbar-logo'>
-        <img src={logo} alt="logo"/>
+        <Link to={"/"}><img src={logo} alt="logo" className='web-logo'/></Link>
       </div>
       <div className='nav-items'>
-        <Link to="/" style={{textDecoration:"none",color:"black"}}><p>Home</p></Link>
-        <Link to="statbyyear" style={{textDecoration:"none",color:"black"}}><p>StatByYear</p></Link>      
+        <NavLink 
+          to="/" 
+          style={({isActive})=>({
+            color:isActive?"black":"white",
+            textDecoration:"none",
+            padding:isActive?"5px 10px":"0px",
+            fontWeight:isActive?"900":"500",
+            background: isActive && "linear-gradient(156deg, rgba(208,203,203,1) 0%, rgba(206,199,199,1) 41%, rgba(255,255,255,1) 59%, rgba(77,77,77,1) 100%)",
+          })}
+          ><p>Home</p></NavLink>
+        <NavLink to="statbyyear" 
+        style={({isActive})=>({
+          color:isActive?"black":"white",
+          textDecoration:"none",
+          padding:isActive?"5px 10px":"0px",
+          fontWeight:isActive?"900":"500",
+          background: isActive && "linear-gradient(156deg, rgba(208,203,203,1) 0%, rgba(206,199,199,1) 41%, rgba(255,255,255,1) 59%, rgba(77,77,77,1) 100%)",
+
+        })}><p>StatByYear</p></NavLink>      
     </div>
     </div>
   )
